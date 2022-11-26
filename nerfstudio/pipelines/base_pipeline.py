4c897a8372d407c0cadfcd92eeff548fb3b57a93
niujinshuchong <yuzh@shanghaitech.edu.cn>
2022-11-26 14:06:10 +0000

    fix multi-gpu training

--- nerfstudio/pipelines/base_pipeline.py
+++ nerfstudio/pipelines/base_pipeline.py
@@ -255,7 +255,7 @@ class VanillaPipeline(Pipeline):
             step: current iteration step to update sampler if using DDP (distributed)
         """
         ray_bundle, batch = self.datamanager.next_train(step)
-        model_outputs = self.model(ray_bundle)
+        model_outputs = self._model(ray_bundle)
         metrics_dict = self.model.get_metrics_dict(model_outputs, batch)
 
         camera_opt_param_group = self.config.datamanager.camera_optimizer.param_group
