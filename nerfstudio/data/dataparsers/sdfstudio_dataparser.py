797811f8253d787f774c677b83cc0d742f399d30
niujinshuchong <yuzh@shanghaitech.edu.cn>
2022-12-07 11:44:50 +0100

    add sdfstudio data parser

---
+++ nerfstudio/data/dataparsers/sdfstudio_dataparser.py
@@ -0,0 +1,250 @@
+# Copyright 2022 The Nerfstudio Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Data parser for friends dataset"""
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Dict, Optional, Type
+
+import numpy as np
+import torch
+from PIL import Image
+from rich.console import Console
+from torchtyping import TensorType
+
+from nerfstudio.cameras.cameras import Cameras, CameraType
+from nerfstudio.data.dataparsers.base_dataparser import (
+    DataParser,
+    DataParserConfig,
+    DataparserOutputs,
+)
+from nerfstudio.data.scene_box import SceneBox
+from nerfstudio.utils.io import load_from_json
+
+CONSOLE = Console()
+
+
+def get_src_from_pairs(
+    ref_idx, all_imgs, pairs_srcs, neighbors_num=None, neighbors_shuffle=False
+) -> Dict[str, TensorType]:
+    # src_idx[0] is ref img
+    src_idx = pairs_srcs[ref_idx]
+    # randomly sample neighbors
+    if neighbors_num and neighbors_num > -1 and neighbors_num < len(src_idx) - 1:
+        if neighbors_shuffle:
+            perm_idx = torch.randperm(len(src_idx) - 1) + 1
+            src_idx = torch.cat([src_idx[[0]], src_idx[perm_idx[:neighbors_num]]])
+        else:
+            src_idx = src_idx[: neighbors_num + 1]
+    src_idx = src_idx.to(all_imgs.device)
+    return {"src_imgs": all_imgs[src_idx], "src_idxs": src_idx}
+
+
+def get_image(image_filename, alpha_color=None) -> TensorType["image_height", "image_width", "num_channels"]:
+    """Returns a 3 channel image.
+
+    Args:
+        image_idx: The image index in the dataset.
+    """
+    pil_image = Image.open(image_filename)
+    np_image = np.array(pil_image, dtype="uint8")  # shape is (h, w, 3 or 4)
+    assert len(np_image.shape) == 3
+    assert np_image.dtype == np.uint8
+    assert np_image.shape[2] in [3, 4], f"Image shape of {np_image.shape} is in correct."
+    image = torch.from_numpy(np_image.astype("float32") / 255.0)
+    if alpha_color is not None and image.shape[-1] == 4:
+        assert image.shape[-1] == 4
+        image = image[:, :, :3] * image[:, :, -1:] + alpha_color * (1.0 - image[:, :, -1:])
+    else:
+        image = image[:, :, :3]
+    return image
+
+
+def get_depths_and_normals(image_idx: int, depths, normals):
+    """function to process additional depths and normal information
+
+    Args:
+        image_idx: specific image index to work with
+        semantics: semantics data
+    """
+
+    # depth
+    depth = depths[image_idx]
+    # normal
+    normal = normals[image_idx]
+
+    return {"depth": depth, "normal": normal}
+
+
+@dataclass
+class SDFStudioDataParserConfig(DataParserConfig):
+    """Scene dataset parser config"""
+
+    _target: Type = field(default_factory=lambda: SDFStudio)
+    """target class to instantiate"""
+    data: Path = Path("data/DTU/scan65")
+    """Directory specifying location of data."""
+    include_mono_prior: bool = False
+    """whether or not to include loading of normal """
+    downscale_factor: int = 1
+    scene_scale: float = 2.0
+    """
+    Sets the bounding cube to have edge length of this size.
+    The longest dimension of the Friends axis-aligned bbox will be scaled to this value.
+    """
+    load_pairs: bool = False
+    """whether to load pairs for multi-view consistency"""
+    neighbors_num: Optional[int] = None
+    neighbors_shuffle: Optional[bool] = False
+    pairs_sorted_ascending: Optional[bool] = True
+    """if src image pairs are sorted in ascending order by similarity i.e. 
+    the last element is the most similar to the first (ref)"""
+
+
+@dataclass
+class SDFStudio(DataParser):
+    """SDFStudio Dataset"""
+
+    config: SDFStudioDataParserConfig
+
+    def _generate_dataparser_outputs(self, split="train"):  # pylint: disable=unused-argument,too-many-statements
+        # load meta data
+        meta = load_from_json(self.config.data / "meta_data.json")
+
+        image_filenames = []
+        depth_images = []
+        normal_images = []
+        fx = []
+        fy = []
+        cx = []
+        cy = []
+        camera_to_worlds = []
+        for frame in meta["frames"]:
+
+            image_filename = self.config.data / frame["rgb_path"]
+
+            intrinsics = torch.tensor(frame["intrinsics"])
+            camtoworld = torch.tensor(frame["camtoworld"])
+
+            # append data
+            image_filenames.append(image_filename)
+            fx.append(intrinsics[0, 0])
+            fy.append(intrinsics[1, 1])
+            cx.append(intrinsics[0, 2])
+            cy.append(intrinsics[1, 2])
+            camera_to_worlds.append(camtoworld)
+
+            if self.config.include_mono_prior:
+                assert meta["has_mono_prior"]
+                # load mono depth
+                depth = np.load(self.config.data / frame["mono_depth_path"])
+                depth_images.append(torch.from_numpy(depth).float())
+
+                # load mono normal
+                normal = np.load(self.config.data / frame["mono_normal_path"])
+
+                # transform normal to world coordinate system
+                normal = normal * 2.0 - 1.0  # omnidata output is normalized so we convert it back to normal here
+                normal = torch.from_numpy(normal).float()
+
+                rot = camtoworld[:3, :3]
+
+                normal_map = normal.reshape(3, -1)
+                normal_map = torch.nn.functional.normalize(normal_map, p=2, dim=0)
+
+                normal_map = rot @ normal_map
+                normal_map = normal_map.permute(1, 0).reshape(*normal.shape[1:], 3)
+                normal_images.append(normal_map)
+
+        fx = torch.stack(fx)
+        fy = torch.stack(fy)
+        cx = torch.stack(cx)
+        cy = torch.stack(cy)
+        camera_to_worlds = torch.stack(camera_to_worlds)
+
+        # Convert from COLMAP's/OPENCV's camera coordinate system to nerfstudio
+        camera_to_worlds[:, 0:3, 1:3] *= -1
+
+        # scene box from meta data
+        meta_scene_box = meta["scene_box"]
+        aabb = torch.tensor(meta_scene_box["aabb"], dtype=torch.float32)
+        scene_box = SceneBox(
+            aabb=aabb,
+            near=meta_scene_box["near"],
+            far=meta_scene_box["far"],
+            radius=meta_scene_box["radius"],
+            collider_type=meta_scene_box["collider_type"],
+        )
+
+        height, width = meta["height"], meta["width"]
+        cameras = Cameras(
+            fx=fx,
+            fy=fy,
+            cx=cx,
+            cy=cy,
+            height=height,
+            width=width,
+            camera_to_worlds=camera_to_worlds[:, :3, :4],
+            camera_type=CameraType.PERSPECTIVE,
+        )
+
+        # TODO supports downsample
+        # cameras.rescale_output_resolution(scaling_factor=1.0 / self.config.downscale_factor)
+
+        if self.config.include_mono_prior:
+            additional_inputs_dict = {
+                "cues": {"func": get_depths_and_normals, "kwargs": {"depths": depth_images, "normals": normal_images}}
+            }
+        else:
+            additional_inputs_dict = {}
+
+        # load pair information
+        pairs_path = self.config.data / "pairs.txt"
+        if pairs_path.exists() and split == "train" and self.config.load_pairs:
+            with open(pairs_path, "r") as f:
+                pairs = f.readlines()
+            split_ext = lambda x: x.split(".")[0]
+            pairs_srcs = []
+            for sources_line in pairs:
+                sources_array = [int(split_ext(img_name)) for img_name in sources_line.split(" ")]
+                if self.config.pairs_sorted_ascending:
+                    # invert (flip) the source elements s.t. the most similar source is in index 1 (index 0 is reference)
+                    sources_array = [sources_array[0]] + sources_array[:1:-1]
+                pairs_srcs.append(sources_array)
+            pairs_srcs = torch.tensor(pairs_srcs)
+            all_imgs = torch.stack(
+                [get_image(image_filename) for image_filename in sorted(image_filenames)], axis=0
+            ).cuda()
+
+            additional_inputs_dict["pairs"] = {
+                "func": get_src_from_pairs,
+                "kwargs": {
+                    "all_imgs": all_imgs,
+                    "pairs_srcs": pairs_srcs,
+                    "neighbors_num": self.config.neighbors_num,
+                    "neighbors_shuffle": self.config.neighbors_shuffle,
+                },
+            }
+
+        dataparser_outputs = DataparserOutputs(
+            image_filenames=image_filenames,
+            cameras=cameras,
+            scene_box=scene_box,
+            additional_inputs=additional_inputs_dict,
+            depths=depth_images,
+            normals=normal_images,
+        )
+        return dataparser_outputs
