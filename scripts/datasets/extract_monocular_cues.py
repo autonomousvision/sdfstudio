9415c2dd85328706ce9e25da1016b975e67cb8d9
niujinshuchong <yuzh@shanghaitech.edu.cn>
2022-12-14 15:12:59 +0100

    scannet preprocess

---
+++ scripts/datasets/extract_monocular_cues.py
@@ -0,0 +1,175 @@
+# adapted from https://github.com/EPFL-VILAB/omnidata
+import argparse
+import glob
+import os.path
+import sys
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import torch
+import torch.nn.functional as F
+from PIL import Image
+from torchvision import transforms
+
+parser = argparse.ArgumentParser(description="Visualize output for depth or surface normals")
+
+parser.add_argument("--omnidata_path", dest="omnidata_path", help="path to omnidata model")
+parser.set_defaults(omnidata_path="/home/yuzh/Projects/omnidata/omnidata_tools/torch/")
+
+parser.add_argument("--pretrained_models", dest="pretrained_models", help="path to pretrained models")
+parser.set_defaults(pretrained_models="/home/yuzh/Projects/omnidata/omnidata_tools/torch/pretrained_models/")
+
+parser.add_argument("--task", dest="task", help="normal or depth")
+parser.set_defaults(task="NONE")
+
+parser.add_argument("--img_path", dest="img_path", help="path to rgb image")
+parser.set_defaults(im_name="NONE")
+
+parser.add_argument("--output_path", dest="output_path", help="path to where output image should be stored")
+parser.set_defaults(store_name="NONE")
+
+args = parser.parse_args()
+
+root_dir = args.pretrained_models
+omnidata_path = args.omnidata_path
+
+sys.path.append(args.omnidata_path)
+print(sys.path)
+from data.transforms import get_transform
+from modules.midas.dpt_depth import DPTDepthModel
+from modules.unet import UNet
+
+trans_topil = transforms.ToPILImage()
+os.system(f"mkdir -p {args.output_path}")
+map_location = (lambda storage, loc: storage.cuda()) if torch.cuda.is_available() else torch.device("cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+# get target task and model
+if args.task == "normal":
+    image_size = 384
+
+    ## Version 1 model
+    # pretrained_weights_path = root_dir + 'omnidata_unet_normal_v1.pth'
+    # model = UNet(in_channels=3, out_channels=3)
+    # checkpoint = torch.load(pretrained_weights_path, map_location=map_location)
+
+    # if 'state_dict' in checkpoint:
+    #     state_dict = {}
+    #     for k, v in checkpoint['state_dict'].items():
+    #         state_dict[k.replace('model.', '')] = v
+    # else:
+    #     state_dict = checkpoint
+
+    pretrained_weights_path = root_dir + "omnidata_dpt_normal_v2.ckpt"
+    model = DPTDepthModel(backbone="vitb_rn50_384", num_channels=3)  # DPT Hybrid
+    checkpoint = torch.load(pretrained_weights_path, map_location=map_location)
+    if "state_dict" in checkpoint:
+        state_dict = {}
+        for k, v in checkpoint["state_dict"].items():
+            state_dict[k[6:]] = v
+    else:
+        state_dict = checkpoint
+
+    model.load_state_dict(state_dict)
+    model.to(device)
+    trans_totensor = transforms.Compose(
+        [
+            transforms.Resize(image_size, interpolation=PIL.Image.BILINEAR),
+            transforms.CenterCrop(image_size),
+            get_transform("rgb", image_size=None),
+        ]
+    )
+
+elif args.task == "depth":
+    image_size = 384
+    pretrained_weights_path = root_dir + "omnidata_dpt_depth_v2.ckpt"  # 'omnidata_dpt_depth_v1.ckpt'
+    # model = DPTDepthModel(backbone='vitl16_384') # DPT Large
+    model = DPTDepthModel(backbone="vitb_rn50_384")  # DPT Hybrid
+    checkpoint = torch.load(pretrained_weights_path, map_location=map_location)
+    if "state_dict" in checkpoint:
+        state_dict = {}
+        for k, v in checkpoint["state_dict"].items():
+            state_dict[k[6:]] = v
+    else:
+        state_dict = checkpoint
+    model.load_state_dict(state_dict)
+    model.to(device)
+    trans_totensor = transforms.Compose(
+        [
+            transforms.Resize(image_size, interpolation=PIL.Image.BILINEAR),
+            transforms.CenterCrop(image_size),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=0.5, std=0.5),
+        ]
+    )
+
+else:
+    print("task should be one of the following: normal, depth")
+    sys.exit()
+
+trans_rgb = transforms.Compose(
+    [
+        transforms.Resize(image_size, interpolation=PIL.Image.BILINEAR),
+        transforms.CenterCrop(image_size),
+    ]
+)
+
+
+def standardize_depth_map(img, mask_valid=None, trunc_value=0.1):
+    if mask_valid is not None:
+        img[~mask_valid] = torch.nan
+    sorted_img = torch.sort(torch.flatten(img))[0]
+    # Remove nan, nan at the end of sort
+    num_nan = sorted_img.isnan().sum()
+    if num_nan > 0:
+        sorted_img = sorted_img[:-num_nan]
+    # Remove outliers
+    trunc_img = sorted_img[int(trunc_value * len(sorted_img)) : int((1 - trunc_value) * len(sorted_img))]
+    trunc_mean = trunc_img.mean()
+    trunc_var = trunc_img.var()
+    eps = 1e-6
+    # Replace nan by mean
+    img = torch.nan_to_num(img, nan=trunc_mean)
+    # Standardize
+    img = (img - trunc_mean) / torch.sqrt(trunc_var + eps)
+    return img
+
+
+def save_outputs(img_path, output_file_name):
+    with torch.no_grad():
+        save_path = os.path.join(args.output_path, output_file_name.replace("_rgb", f"_{args.task}") + ".png")
+        print(f"Reading input {img_path} ...")
+        img = Image.open(img_path)
+
+        img_tensor = trans_totensor(img)[:3].unsqueeze(0).to(device)
+
+        if img_tensor.shape[1] == 1:
+            img_tensor = img_tensor.repeat_interleave(3, 1)
+
+        output = model(img_tensor).clamp(min=0, max=1)
+
+        if args.task == "depth":
+            # output = F.interpolate(output.unsqueeze(0), (512, 512), mode='bicubic').squeeze(0)
+            output = output.clamp(0, 1)
+
+            np.save(save_path.replace(".png", ".npy"), output.detach().cpu().numpy()[0])
+            plt.imsave(save_path, output.detach().cpu().squeeze(), cmap="viridis")
+        else:
+            np.save(save_path.replace(".png", ".npy"), output.detach().cpu().numpy()[0])
+            trans_topil(output[0]).save(save_path)
+
+        print(f"Writing output {save_path} ...")
+
+
+img_path = Path(args.img_path)
+if img_path.is_file():
+    save_outputs(args.img_path, os.path.splitext(os.path.basename(args.img_path))[0])
+elif img_path.is_dir():
+    for f in glob.glob(args.img_path + "/*_rgb.png"):
+        save_outputs(f, os.path.splitext(os.path.basename(f))[0])
+else:
+    print("invalid file path!")
+    sys.exit()
---
+++ scripts/datasets/process_scannet_to_sdfstudio.py
@@ -0,0 +1,136 @@
+import argparse
+import glob
+import json
+import os
+from pathlib import Path
+
+import numpy as np
+import PIL
+from PIL import Image
+from torchvision import transforms
+
+parser = argparse.ArgumentParser(description="preprocess scannet dataset to sdfstudio dataset")
+
+parser.add_argument("--input_path", dest="input_path", help="path to scannet scene")
+parser.set_defaults(im_name="NONE")
+
+parser.add_argument("--output_path", dest="output_path", help="path to output")
+parser.set_defaults(store_name="NONE")
+
+args = parser.parse_args()
+
+image_size = 384
+trans_totensor = transforms.Compose(
+    [
+        transforms.CenterCrop(image_size * 2),
+        transforms.Resize(image_size, interpolation=PIL.Image.BILINEAR),
+    ]
+)
+
+output_path = Path(args.output_path)  # "data/custom/scannet_scene0050_00"
+input_path = Path(args.input_path)  # "/home/yuzh/Projects/datasets/scannet/scene0050_00"
+
+output_path.mkdir(parents=True, exist_ok=True)
+
+# load color
+color_path = input_path / "frames" / "color"
+color_paths = sorted(glob.glob(os.path.join(color_path, "*.jpg")), key=lambda x: int(os.path.basename(x)[:-4]))
+
+# load intrinsic
+intrinsic_path = input_path / "frames" / "intrinsic" / "intrinsic_color.txt"
+camera_intrinsic = np.loadtxt(intrinsic_path)
+
+# load pose
+pose_path = input_path / "frames" / "pose"
+poses = []
+pose_paths = sorted(glob.glob(os.path.join(pose_path, "*.txt")), key=lambda x: int(os.path.basename(x)[:-4]))
+for pose_path in pose_paths:
+    c2w = np.loadtxt(pose_path)
+    poses.append(c2w)
+poses = np.array(poses)
+
+# deal with invalid poses
+valid_poses = np.isfinite(poses).all(axis=2).all(axis=1)
+min_vertices = poses[:, :3, 3][valid_poses].min(axis=0)
+max_vertices = poses[:, :3, 3][valid_poses].max(axis=0)
+
+center = (min_vertices + max_vertices) / 2.0
+scale = 2.0 / (np.max(max_vertices - min_vertices) + 3.0)
+print(center, scale)
+
+# we should normalize pose to unit cube
+poses[:, :3, 3] -= center
+poses[:, :3, 3] *= scale
+
+# inverse normalization
+scale_mat = np.eye(4).astype(np.float32)
+scale_mat[:3, 3] -= center
+scale_mat[:3] *= scale
+scale_mat = np.linalg.inv(scale_mat)
+
+# copy image
+H, W = 968, 1296
+
+# center crop by 2 * image_size
+offset_x = (W - image_size * 2) * 0.5
+offset_y = (H - image_size * 2) * 0.5
+camera_intrinsic[0, 2] -= offset_x
+camera_intrinsic[1, 2] -= offset_y
+# resize from 384*2 to 384
+resize_factor = 0.5
+camera_intrinsic[:2, :] *= resize_factor
+
+K = camera_intrinsic
+
+frames = []
+out_index = 0
+for idx, (valid, pose, image_path) in enumerate(zip(valid_poses, poses, color_paths)):
+
+    if idx % 10 != 0:
+        continue
+    if not valid:
+        continue
+
+    target_image = output_path / f"{out_index:06d}_rgb.png"
+    print(target_image)
+    img = Image.open(image_path)
+    img_tensor = trans_totensor(img)
+    img_tensor.save(target_image)
+
+    rgb_path = str(target_image.relative_to(output_path))
+    frame = {
+        "rgb_path": rgb_path,
+        "camtoworld": pose.tolist(),
+        "intrinsics": K.tolist(),
+        "mono_depth_path": rgb_path.replace("_rgb.png", "_depth.npy"),
+        "mono_normal_path": rgb_path.replace("_rgb.png", "_normal.npy"),
+    }
+
+    frames.append(frame)
+    out_index += 1
+
+# scene bbox for the scannet scene
+scene_box = {
+    "aabb": [[-1, -1, -1], [1, 1, 1]],
+    "near": 0.05,
+    "far": 2.5,
+    "radius": 1.0,
+    "collider_type": "box",
+}
+
+# meta data
+output_data = {
+    "camera_model": "OPENCV",
+    "height": image_size,
+    "width": image_size,
+    "has_mono_prior": True,
+    "pairs": None,
+    "worldtogt": scale_mat.tolist(),
+    "scene_box": scene_box,
+}
+
+output_data["frames"] = frames
+
+# save as json
+with open(output_path / "meta_data.json", "w", encoding="utf-8") as f:
+    json.dump(output_data, f, indent=4)
