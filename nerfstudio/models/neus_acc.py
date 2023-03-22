a10a5d71de24ad2cc5c01456302f3d4feb74e050
niujinshuchong <yuzh@shanghaitech.edu.cn>
2023-03-22 14:37:45 +0100

    add normal vis

--- nerfstudio/models/neus_acc.py
+++ nerfstudio/models/neus_acc.py
@@ -138,6 +138,8 @@ class NeuSAccModel(NeuSModel):
             if self.training:
                 outputs.update({"eik_grad": zeros})
 
+        # this is used only in viewer
+        outputs["normal_vis"] = (outputs["normal"] + 1.0) / 2.0
         return outputs
 
     def get_metrics_dict(self, outputs, batch):
