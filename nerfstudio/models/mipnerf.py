00d4e8dac87fd02d613b83a72fc03729cd92c875
Stefano Esposito <stefano.esposito97@outlook.com>
2023-07-12 10:56:41 +0200

    torchmetrics was deprecated, importing  from  instead

--- nerfstudio/models/mipnerf.py
+++ nerfstudio/models/mipnerf.py
@@ -21,7 +21,7 @@ from typing import Dict, List, Tuple
 
 import torch
 from torch.nn import Parameter
-from torchmetrics import PeakSignalNoiseRatio
+from torchmetrics.image import PeakSignalNoiseRatio
 from torchmetrics.functional import structural_similarity_index_measure
 from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
 
